02ac616aeafdcd1f21ccc7d1e4e1f08028013e7d
lidabzzz <lida.byzina@gmail.com>
2024-02-05 14:30:23 -0800

    Add meta tags

---
+++ src/Footer.js
@@ -0,0 +1,29 @@
+function Footer() {
+    return (
+        <footer>
+            <img src="/images/logo.svg" alt="company logo"/>
+            <ul>
+                <h5>Doormat Navigation</h5>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Order Online</li>
+                <li>Login</li></ul>
+            <ul>
+                <h5>Contact</h5>
+                <li>Address</li>
+                <li>Phone number</li>
+                <li>Email</li>
+            </ul>
+            <ul>
+                <h5>Social Media links</h5>
+                <li><a href="facebook.com">Facebook</a></li>
+                <li><a href="instagram.com">Instagram</a></li>
+                <li><a href="yelp.com">Yelp</a></li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+
+function Header() {
+    return (
+        <header>
+            <img src="/images/logo.svg" alt="company logo"/>
+        </header>
+    )
+}
+
+export default Header;
