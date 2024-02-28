06651b132d63a01475be91a7aa6c5e1f4222fbfd
lidabzzz <lida.byzina@gmail.com>
2024-02-27 16:53:07 -0800

    Change Reservation UI

--- src/Reservation.js
+++ src/Reservation.js
@@ -1,16 +1,24 @@
 function Reservation() {
     return(
-        <div>
-            Chucky
-            {/* <div>
-                <div>
-
-                </div>
+        <section className="reservations-main">
+            <div className="reservations">
+                <h3>Little Lemon Reservation Form</h3>
                 <form>
-
+                    <label>
+                        Name:
+                        <input type="text" name="name"/>
+                    </label>
+                    <label>
+                        Number of guests:
+                        <input type="number" name="numberOfGuests" min={1} max={10} defaultValue={4}/>
+                    </label>
+                    <label>
+                        Email:
+                        <input type="email" name="email"/>
+                    </label>
                 </form>
-            </div> */}
-        </div>
+            </div>
+        </section>
     )
 }
 
